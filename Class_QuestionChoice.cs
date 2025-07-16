afdf8fcd51925edd1d536e42b9cbb065e3ed7e62
Sleeperbix <peter.lindley@outlook.com>
2025-07-16 17:49:45 +0100

    first commit

---
+++ Class_QuestionChoice.cs
@@ -0,0 +1,16 @@
+public class Class_QuestionChoice
+{
+    public string CategoryTitle; // Title of the category, used for the question selection scene as the name that appears on the button.
+    public string QuestionText;
+    public string AnswerA;
+    public string AnswerB;
+    public string AnswerC;
+    public string AnswerD;
+    public string CorrectAnswer;
+    public string AnswerFact; // Text to accompany the correct answer, elaborating on why the answer is correct etc.
+// Following strings are paths to files.
+    public string QuestionImage; // Image that appears during the question. Will be scaled into a square shape.
+    public string BGImage;
+    public string SelectionSound; // Sound that plays when the question is selected in the question selection scene.
+    public string BGMusic;
+}
